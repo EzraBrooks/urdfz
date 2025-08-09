cb6bc459fb79176b2a6fc7bc8009f7bdafca87ec
Ezra Brooks <ezra@brooks.cx>
2025-08-09 17:51:43 -0600

    Simple test to prove deps are all working

--- pack.py
+++ pack.py
@@ -1,1 +1,6 @@
+from pathlib import Path
 
+
+def read_file_to_str(path: Path | str) -> str:
+    with open(path, "r") as f:
+        return f.read()
--- pack_test.py
+++ pack_test.py
@@ -1,1 +1,13 @@
+from pack import read_file_to_str
+from unittest.mock import patch, mock_open
+from pathlib import Path
 
+
+def test_read_file_to_str():
+    with patch("builtins.open", mock_open(read_data="data")) as mock_file:
+        assert read_file_to_str("path/to/my/file") == "data"
+        mock_file.assert_called_with("path/to/my/file", "r")
+    with patch("builtins.open", mock_open(read_data="data2")) as mock_file:
+        path = Path("path/to/my/file2")
+        assert read_file_to_str(path) == "data2"
+        mock_file.assert_called_with(path, "r")

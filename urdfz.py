d34a3c1dd24f3aa57444f742f7c1b384bf9b69be
Ezra Brooks <ezra@brooks.cx>
2025-08-09 17:23:04 -0600

    Add initial ruff, ty, pytest configs

--- urdfz.py
+++ urdfz.py
@@ -3,15 +3,22 @@ from typing_extensions import Annotated
 
 cli = typer.Typer()
 
+
 @cli.command()
-def pack(path: Annotated[str, typer.Argument(help="The path to the URDF file to pack")]):
+def pack(
+    path: Annotated[str, typer.Argument(help="The path to the URDF file to pack")],
+):
     """Packs a URDF and all its assets into a URDFZ file."""
     raise NotImplementedError()
 
+
 @cli.command()
-def unpack(path: Annotated[str, typer.Argument(help="The path to the URDFZ file to unpack")]):
+def unpack(
+    path: Annotated[str, typer.Argument(help="The path to the URDFZ file to unpack")],
+):
     """Unpacks a URDFZ into its component URDF file and assets."""
     raise NotImplementedError()
+
 
 if __name__ == "__main__":
     cli()
